5c3ca0eacf1e98ee01b33ad9f26d7858ebeeee78
Anton <>
2023-01-25 10:18:34 +0100

    Add project files.

---
+++ Uppgift 2.6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Skriv in 2 tal");
+            Console.Write("Tal 1 : ");
+            double tal1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Tal 2 : ");
+            double tal2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Medelvärdet är "+((tal1+tal2)/2));
+
+
+        }
+    }
+}
